1ba56bfd7b98dacd0580b75119f8640a68ff646b
Sai Akhil Kumar Reddy Nallamilli <190031153@kluniversity.in>
2020-12-28 14:08:57 +0530

    removed the comment lines and added the recpatcha new code

--- index.js
+++ index.js
@@ -36,13 +36,9 @@ app.use(
     })
 );
 
-// app.use(express.static(__dirname + '/whin/dist/whin'));
-// app.get('/*', function(req,res) {
-//     res.sendFile(path.join(__dirname+'/whin/dist/whin/index.html'));
-// });
-
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/booktravel',booktravelRoute);
 app.use('/booktour',booktourRoute);
 app.use('/bookhospitality',bookhospitalityRoute);
@@ -50,24 +46,45 @@ app.use('/donate',donateRoute);
 app.use('/contactus',contactusRoute);
 app.use('/user',userRoute);
 
-app.post('/verify', (req,res) => {
-  var secretKey = '6LfiBhQaAAAAALj72o9LBq7ruMMJe_WgCDIYK8WZ';
-  var userKey = req.body.token;
-  axios.post('https://www.google.com/recaptcha/api/siteverify?secret='+secretKey+'&response='+userKey).then(response => {
-      console.log("got response ", response.data)
-      if(response.data.success) {
-          return res.status(200).json({
-              response: "Verification Successful"
-          });
-      }
-      return res.status(401).json({
-          error: "Verification Failed"
-      });
-      }).catch(error => {
-          res.status(500).json({
-            error: "Server Not Responding"  
-          })
-  });
+// app.post('/verify', (req,res) => {
+//     var secretKey = '6LfiBhQaAAAAALj72o9LBq7ruMMJe_WgCDIYK8WZ';
+//     var userKey = req.body.token;
+//     axios.post('https://www.google.com/recaptcha/api/siteverify?secret='+secretKey+'&response='+userKey).then(response => {
+//         console.log("got response ", response.data)
+//         if(response.data.success) {
+//             return res.status(200).json({
+//                 response: "Verification Successful"
+//             });
+//         }
+//         return res.status(401).json({
+//             error: "Verification Failed"
+//         });
+//         }).catch(error => {
+//             res.status(500).json({
+//               error: "Server Not Responding"  
+//             })
+//     });
+// });
+
+app.post('/verify', (req, res)=>{
+      
+  let token = req.body.recaptcha;
+  const secretKey = "6LfiBhQaAAAAALj72o9LBq7ruMMJe_WgCDIYK8WZ";
+  
+  const url =  `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}&remoteip=${req.connection.remoteAddress}`    
+  if(token === null || token === undefined){
+    res.status(201).send({success: false, message: "Token is empty or invalid"})
+    return console.log("token empty");
+  }
+  
+  request(url, function(err, response, body){
+    body = JSON.parse(body);
+    if(body.success !== undefined && !data.success){
+         res.send({success: false, 'message': "recaptcha failed"});
+         return console.log("failed")
+     }
+     res.send({"success": true, 'message': "recaptcha passed"});
+  })
 });
 
 app.get("*", (req, res) => { 
